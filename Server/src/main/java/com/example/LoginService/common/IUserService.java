fc9e954627bb065986e6b19b77abfd320af73b6b
metalfist213 <silverleaves13@gmail.com>
2019-05-22 15:40:03 +0200

    Friendsstuff added

--- Server/src/main/java/com/example/LoginService/common/IUserService.java
+++ Server/src/main/java/com/example/LoginService/common/IUserService.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 public interface IUserService {
     public ResponseEntity<User> getUser(String id);
-    public ResponseEntity<List<User>> getFriendList(User user);
+    public ResponseEntity<List<User>> getAllFriends(User user);
     public ResponseEntity<String> updateUser(User user);
-    ResponseEntity<String> addFriend(User user, String friend_id);
+    ResponseEntity<Boolean> addFriend(User user, String email);
+    ResponseEntity<User> getFriend(User user, String email);
     ResponseEntity<String> removeString(User user, String friend_id);
 
 }
