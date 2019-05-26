3da73c304dc88e71776a5f7eddc8f4d1f262ff8e
Marc226 <Marc.g.nielsen@gmail.com>
2019-05-26 15:27:28 +0200

    updated to mdpi

--- Private/Main/src/main/java/com/example/main/utils/ApplicationConfig.java
+++ Private/Main/src/main/java/com/example/main/utils/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package com.example.main.utils;
 
 public class ApplicationConfig {
-    //TODO:Update backend to the login system
     public final String WEBSERVICE_URL = "http://139.59.149.71:8999/";
 }
